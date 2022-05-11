c32074d8a40af3cac66d89882ec9bec950df4501
symon254 <79975499+symon254@users.noreply.github.com>
2022-05-11 16:00:06 +0300

    refactor: modification
    component modiication

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,33 +1,27 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const buttonOutline = {
-  primary: "btn-outline-primary",
-  secondary: "btn-outline-secondary",
-  success: "btn-outline-success",
-  danger: "btn-outline-danger",
-  info: "btn-outline-info",
-  warning: "btn-outline-warning",
+  primary: "btn btn-outline-primary",
+  secondary: "btn btn-outline-secondary",
+  success: "btn btn-outline-success",
+  danger: "btn btn-outline-danger",
+  info: "btn btn-outline-info",
+  warning: "btn btn-outline-warning",
 };
 
 const buttonFilled = {
-  primary: "btn-primary",
-  secondary: "btn-secondary",
-  success: "btn-success",
-  danger: "btn-danger",
-  info: "btn-info",
-  warning: "btn-warning",
-};
-const buttonSize = {
-  lg: "btn-lg",
-  sm: "btn-sm",
+  primary: "btn btn-primary",
+  secondary: "btn btn-secondary",
+  success: "btn btn-success",
+  danger: "btn btn-danger",
+  info: "btn btn-info",
+  warning: "btn btn-warning",
 };
 
 export const Button = ({
   className,
   children,
   type,
-  size,
   button,
   buttonType,
   ...rest
@@ -36,41 +30,20 @@ export const Button = ({
 
   const btnOutline = [buttonOutline[button]];
   const btnFilled = [buttonFilled[button]];
-  const btnSize = [buttonSize[size]];
-
-  if (size === "sm") {
-    classes.push(...btnSize);
-  } else if (size === "lg") {
-    classes.push(...btnSize);
-  }
 
   if (buttonType === "outline") {
     classes.push(...btnOutline);
   } else if (buttonType === "filled") {
     classes.push(...btnFilled);
-  } else {
-    classes.push(...btnFilled);
   }
 
   classes = classes.join(" ");
+
   return (
-    <div className="col">
+    <div className="col-3">
       <button {...rest} type={type} className={`btn ${classes} ${className}`}>
         {children}
       </button>
     </div>
   );
-};
-
-Button.defaultProps = {
-  type: "button",
-  button: "primary",
-  buttonType: "filled",
-};
-
-Button.propTypes = {
-  children: PropTypes.node.isRequired,
-  button: PropTypes.string.isRequired,
-  buttonType: PropTypes.string.isRequired,
-  size: PropTypes.string,
 };
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,30 +3,11 @@ import React from "react";
 
 const Card = ({ header, title, description }) => {
   return (
-    // <div className="card rounded-5 pb-3 mb-4">
-    //   <h5 className="card-header">{header}</h5>
-    //   <div className="card-body">
-    //     <h5 className="card-title">{title}</h5>
-    //     <span className="card-text">{description}</span>
-    //   </div>
-    // </div>
-
-    <div className="card rounded-5 p-3 mb-2">
-      <div className="d-flex justify-content-between">
-        <div className="d-flex flex-row align-items-center">
-          <div className="icon">
-            <i className="bx bxl-dribbble" />
-          </div>
-          <div className="ms-2 c-details">
-            <h4 className="mb-0">{header}</h4>
-          </div>
-        </div>
-        <div className="badge">
-          <span className="px-1">Components</span>
-        </div>
-      </div>
-      <div className="mt-4">
-        <h3 className="heading">{description}</h3>
+    <div className="card pb-3 mb-4">
+      <h5 className="card-header">{header}</h5>
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <span className="card-text">{description}</span>
       </div>
     </div>
   );
