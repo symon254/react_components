333218b6de9cc6a55015e2543cac6884588b2306
symon254 <79975499+symon254@users.noreply.github.com>
2022-05-11 16:00:45 +0300

    refactor: .
    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import Layout from "./Container/Layout";
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Layout />
-      </Router>
-    </>
+    <div >
+      <Layout />
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
-import $ from "jquery";
-import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Suspense fallback="Loading...">
-      <App />
-    </Suspense>
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
