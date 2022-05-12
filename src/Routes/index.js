3d283ae7b077469ad695f6a4a6116742c30243f6
symon254 <79975499+symon254@users.noreply.github.com>
2022-05-13 00:58:59 +0300

    feat(index.js): page component
    adding the page component

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,16 +2,21 @@ import { lazy } from "react";
 
 const Home = lazy(() => import("../Screens/Home/Home"));
 const PageA = lazy(() => import("../Screens/PageA/PageA"));
+const PageB = lazy(() => import("../Screens/PageB/PageB"));
 
 const screens = [
-  {
-    component: Home,
-    path: "/",
-  },
-  {
-    component: PageA,
-    path: "/pageA",
-  },
+    {
+        component: Home,
+        path: "/",
+    },
+    {
+        component: PageA,
+        path: "/pageA",
+    },
+    {
+        component: PageB,
+        path: "/PageB",
+    },
 ];
 
 export default screens;
