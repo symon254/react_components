dc74d0e634f92c3e7fcd8a721c5ae019270a7838
symon254 <79975499+symon254@users.noreply.github.com>
2022-05-13 00:57:54 +0300

    feat(Layout.js): .
    .

--- src/Container/Layout.js
+++ src/Container/Layout.js
@@ -4,33 +4,33 @@ import { Navbar } from "../Components/Navbar/Navbar";
 import screens from "../Routes";
 
 const layout = () => {
-  return (
-    <>
-      <Navbar />
-      <div className="content-wrapper">
-        <div className="container mt-5 mb-3">
-          <Routes>
-            {screens.map((item, i) => {
-              return item.component ? (
-                <Route
-                  exact
-                  key={i}
-                  path={item.path}
-                  element={
-                    <Suspense fallback="Loading...">
-                      <item.component />
-                    </Suspense>
-                  }
-                />
-              ) : (
-                "null"
-              );
-            })}
-          </Routes>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <div className="content-wrapper">
+                <div className="container mt-5 mb-3">
+                    <Routes>
+                        {screens.map((item, i) => {
+                            return item.component ? (
+                                <Route
+                                    exact
+                                    key={i}
+                                    path={item.path}
+                                    element={
+                                        <Suspense fallback="Loading...">
+                                            <item.component />
+                                        </Suspense>
+                                    }
+                                />
+                            ) : (
+                                "null"
+                            );
+                        })}
+                    </Routes>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default layout;
